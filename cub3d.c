614e1a5f09fed5e70fd1b97345c8f81e9330065f
Jacopo Lombardo <jalombar@c3a3c7.42berlin.de>
2025-02-08 12:41:26 +0100

    parser

--- cub3d.c
+++ cub3d.c
@@ -6,15 +6,53 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 11:05:40 by jalombar          #+#    #+#             */
-/*   Updated: 2025/02/07 11:32:02 by jalombar         ###   ########.fr       */
+/*   Updated: 2025/02/08 12:38:48 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3d.h"
 
-void	ft_cub3d(char *map)
+void	ft_print_result(t_map *map)
 {
-	
+	int	i;
+
+	i = 0;
+	ft_printf("NO: %s\n", map->no);
+	ft_printf("SO: %s\n", map->so);
+	ft_printf("WE: %s\n", map->we);
+	ft_printf("EA: %s\n", map->ea);
+	ft_printf("Ceiling: %s\n", map->c);
+	ft_printf("Floor: %s\n", map->f);
+	ft_printf("\nMAP: \n\n");
+	while (map->map[i])
+	{
+		ft_printf("%s\n", map->map[i]);
+		i++;
+	}
+}
+
+void	ft_map_init(t_map *map)
+{
+	char	**mapx;
+
+	mapx = (char **)malloc(1 * sizeof(char *));
+	mapx[0] = NULL;
+	map->map = mapx;
+	map->no = NULL;
+	map->so = NULL;
+	map->we = NULL;
+	map->ea = NULL;
+	map->c = NULL;
+	map->f = NULL;
+}
+
+void	ft_cub3d(char *input)
+{
+	t_map	map;
+
+	ft_map_init(&map);
+	ft_parser(input, &map);
+	ft_print_result(&map);
 }
 
 int	main(int argc, char **argv)
--- source/parser_utils.c
+++ source/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 14:48:51 by jalombar          #+#    #+#             */
-/*   Updated: 2025/02/07 14:49:11 by jalombar         ###   ########.fr       */
+/*   Updated: 2025/02/08 12:36:29 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_strlen2(char *line, int i)
 	int	len;
 
 	len = 0;
-	while (line[i] && line[i] != ' ')
+	while (line[i] && line[i] != '\n' && line[i] != ' ')
 	{
 		i++;
 		len++;
