e2fe3ed7be8015015f3a793b456f011404e93645
Nick Boer <nboer@c3a3c8.42berlin.de>
2025-02-20 14:24:10 +0100

    update

--- source/libx/events.c
+++ source/libx/events.c
@@ -6,7 +6,7 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 11:34:45 by nboer             #+#    #+#             */
-/*   Updated: 2025/02/20 13:30:15 by nboer            ###   ########.fr       */
+/*   Updated: 2025/02/20 14:23:14 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ int	ft_events_keyboard(int keycode, t_data *data)
 		ft_event_close_win(data);
 	if (keycode == 119)
 	{
-		data->player->pos_x += 30;
-		data->player->pos_y += 30;
+		data->player->pos_x += data->player->dir_x * 5;
+		data->player->pos_y += data->player->dir_y * 5;
 		printf("player position adjusted\n");
 		mlx_clear_window(data->mlx, data->win);
 		ft_init_rays(data);
-		mlx_put_image_to_window(data->mlx, data->win, data->image->img, WIDTH, HEIGHT);
-		print_image_info(data->image);
+		mlx_put_image_to_window(data->mlx, data->win, data->image->img, 0, 0);
+		// print_image_info(data->image);
 	}
 	if (keycode == XK_S)
 		data->player->mv_back = 1;
