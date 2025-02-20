af43b40763bec489f1274e6e3a14d6a349d613da
Nick Boer <nboer@c3a3c8.42berlin.de>
2025-02-20 16:19:00 +0100

    update

--- source/libx/events.c
+++ source/libx/events.c
@@ -6,11 +6,26 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 11:34:45 by nboer             #+#    #+#             */
-/*   Updated: 2025/02/20 14:47:15 by nboer            ###   ########.fr       */
+/*   Updated: 2025/02/20 16:13:13 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+void	ft_rotate_player(t_player *player, double angle_increment)
+{
+	double	old_dir_x;
+	double	old_dir_y;
+	double	cos_incr_angle;
+	double	sin_incr_angle;
+
+	old_dir_x = player->dir_x;
+	old_dir_y = player->dir_y;
+	cos_incr_angle = cos(angle_increment);
+	sin_incr_angle = sin(angle_increment);
+	player->dir_x = old_dir_x * cos_incr_angle - old_dir_y * sin_incr_angle;
+	player->dir_y = old_dir_x * sin_incr_angle + old_dir_y * cos_incr_angle;
+}
 
 int	ft_event_close_win(t_data *data)
 {
@@ -30,29 +45,21 @@ int	ft_events_keyboard(int keycode, t_data *data)
 	printf("Key pressed: %d\n", keycode);
 	if (keycode == XK_Escape)
 		ft_event_close_win(data);
-	if (keycode == 119)
+	if (keycode == XK_w || keycode == XK_Up)
 	{
-		data->player->pos_x += data->player->dir_x;
-		data->player->pos_y += data->player->dir_y;
-		ft_render_screen(data);
-		// print_image_info(data->image);
+		data->player->mv_forward = 1;
+		data->player->pos_x += data->player->dir_x * 3;
+		data->player->pos_y += data->player->dir_y * 3;
 	}
-	if (keycode == XK_S)
-		data->player->mv_back = 1;
+	if (keycode == XK_d || keycode == XK_Right)
+		ft_rotate_player(data->player, ft_dtor(3.0));
+	if (keycode == XK_a || keycode == XK_Left)
+		ft_rotate_player(data->player, ft_dtor(-3.0));
+	if (keycode == XK_s || keycode == XK_Down)
+	{
+		data->player->pos_x -= data->player->dir_x * 3;
+		data->player->pos_y -= data->player->dir_y * 3;
+	}
+	ft_render_screen(data);
 	return (0);
 }
-
-
-// void	update_game(t_data *data, t_player *player)
-// {
-// 	if (player->mv_forward)
-// 	{
-// 		player->pos_x += player->dir_x;
-// 		player->pos_y += player->dir_y;
-// 	}
-// 	if (player->mv_back)
-// 	{
-// 		player->pos_x -= player->dir_x;
-// 		player->pos_y -= player->dir_y;
-// 	}
-// }
